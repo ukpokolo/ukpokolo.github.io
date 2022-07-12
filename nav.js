26c2ac65ecf18ddcacd5584b4fda483787f50e5b
ukpokolo <89245095+ukpokolo@users.noreply.github.com>
2022-07-12 12:56:46 +0100

    update

--- nav.js
+++ nav.js
@@ -1,29 +1,37 @@
-const primaryNav = document.querySelector(".nav-list");
-const navToggle = document.querySelector(".mobile-nav-toggle");
-const navLinks = document.querySelector(".nav-list li");
+// const primaryNav = document.querySelector(".nav-list");
+// const navToggle = document.querySelector(".mobile-nav-toggle");
+// const navLinks = document.querySelector(".nav-list li");
 
-navToggle.addEventListener('click', () => {
-    const visibility = primaryNav.getAttribute('data-visible')
+// navToggle.addEventListener('click', () => {
+//     const visibility = primaryNav.getAttribute('data-visible')
 
-    if(visibility === "false"){
-        primaryNav.setAttribute("data-visible", true);
-        navToggle.setAttribute("data-visible", true)
-    } else if (visibility === "true"){
-        primaryNav.setAttribute("data-visible", false);
-        navToggle.setAttribute("data-visible", false)
-    };
+//     if(visibility === "false"){
+//         primaryNav.setAttribute("data-visible", true);
+//         navToggle.setAttribute("data-visible", true)
+//     } else if (visibility === "true"){
+//         primaryNav.setAttribute("data-visible", false);
+//         navToggle.setAttribute("data-visible", false)
+//     };
 
-    navLinks.forEach((link, index) => {
-        console.log(index);
-    });
     
-}) 
+    
+// }) 
 
-// const navSlide = () => {
-//     const burger = document.querySelector('.burger');
-//     const nav = document.querySelector('.nav-list');
 
-//     burger.addEventListener('click', () => {
-//         nav.classList.toggle('nav-active');
-//     });
-// }
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-list');
+    const navLinks = document.querySelectorAll('.nav-list li');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        burger.classList.toggle('toggle');
+    });
+
+
+
+}
+
+navSlide();

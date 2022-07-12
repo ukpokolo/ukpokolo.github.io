41a6a13917ce364ba4ceca6936590e062d5c1349
ukpokolo <89245095+ukpokolo@users.noreply.github.com>
2022-07-12 04:10:46 +0100

    update

--- nav.js
+++ nav.js
@@ -1,5 +1,6 @@
 const primaryNav = document.querySelector(".nav-list");
 const navToggle = document.querySelector(".mobile-nav-toggle");
+const navLinks = document.querySelector(".nav-list li");
 
 navToggle.addEventListener('click', () => {
     const visibility = primaryNav.getAttribute('data-visible')
@@ -10,5 +11,19 @@ navToggle.addEventListener('click', () => {
     } else if (visibility === "true"){
         primaryNav.setAttribute("data-visible", false);
         navToggle.setAttribute("data-visible", false)
-    }
+    };
+
+    navLinks.forEach((link, index) => {
+        console.log(index);
+    });
+    
 }) 
+
+// const navSlide = () => {
+//     const burger = document.querySelector('.burger');
+//     const nav = document.querySelector('.nav-list');
+
+//     burger.addEventListener('click', () => {
+//         nav.classList.toggle('nav-active');
+//     });
+// }
